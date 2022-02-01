4941a6a3ab8a9a24100ca6db813ccae6f637a4d4
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-01 17:03:02 +0200

    created District model,Controller and migration

---
+++ app/Http/Controllers/DistrictController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\District;
+use Illuminate\Http\Request;
+
+class DistrictController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\District  $district
+     * @return \Illuminate\Http\Response
+     */
+    public function show(District $district)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\District  $district
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(District $district)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\District  $district
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, District $district)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\District  $district
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(District $district)
+    {
+        //
+    }
+}
