197eb53df240062eab737e0b2dfb2ad0824cf090
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-03 11:27:13 +0200

    Task (Cell Endpoints) : Created Cell Controller Model Migration

---
+++ app/Http/Controllers/CellController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cell;
+use Illuminate\Http\Request;
+
+class CellController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cell  $cell
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cell $cell)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cell  $cell
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cell $cell)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cell  $cell
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cell $cell)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cell  $cell
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cell $cell)
+    {
+        //
+    }
+}
