da4e539ea0bb538ea55ecdf3d572ba5f21e2a435
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-03 11:46:58 +0200

    Chore (Village Methods) : added Villaged Create method Read method Update method Delete method

--- app/Http/Controllers/VillageController.php
+++ app/Http/Controllers/VillageController.php
@@ -7,79 +7,69 @@ use Illuminate\Http\Request;
 
 class VillageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $Sectors = Village::get(['villageName']);
+        return response()->json(
+            $Sectors,
+            200
+        );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $newVillage = new Village();
+        $newVillage->cell_id = $request['cell_id'];
+        $newVillage->villageName = $request['villageName'];
+        $newVillage->save();
+        return response()->json([
+            'message' => 'Village Registered',
+            'data' => ['Village' => $newVillage]
+        ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Village  $village
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Village $village)
+    public function show($id)
     {
-        //
+        $currentVillage = Village::find($id);
+        if ($currentVillage) {
+            return response()->json(
+                $currentVillage,
+                200
+            );
+        }
+        return response()->json([
+            'message' => 'No Village Found'
+        ], 404);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Village  $village
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Village $village)
+    public function update(Request $request, $id)
     {
-        //
+        $currentVillage =  Village::find($id);
+        if ($currentVillage) {
+            $currentVillage->cell_id = $request['cell_id'];
+            $currentVillage->villageName = $request["villageName"];
+            $currentVillage->save();
+            return response()->json([
+                'message' => 'Village Update',
+                'Village' => [$currentVillage]
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'No Village Found'
+        ], 404);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Village  $village
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Village $village)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Village  $village
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Village $village)
-    {
-        //
+        $currentVillage =  Village::find($id);
+        if ($currentVillage) {
+            $currentVillage->delete();
+            return response()->json([
+                'message' => 'Village Deleted Successfully',
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'No Village Found'
+        ], 404);
     }
 }
