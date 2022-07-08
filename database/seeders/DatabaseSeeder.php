6b9c2b6dfef2c9f661e372c4a0afd7a9c67e5e9f
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-08 18:01:17 +0200

    ft(Seeder): added district seeder to db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(ProvinceSeeder::class);
+        $this->call(DistrictSeeder::class);
     }
 }
