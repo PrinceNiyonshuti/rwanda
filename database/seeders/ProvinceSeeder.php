71cfca8c715c5f68038bd4d3992f9947ef7d2c37
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-08 17:54:02 +0200

    ft(Province): added default province data

--- database/seeders/ProvinceSeeder.php
+++ database/seeders/ProvinceSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Province;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class ProvinceSeeder extends Seeder
 {
@@ -14,5 +16,13 @@ class ProvinceSeeder extends Seeder
     public function run()
     {
         //
+        Schema::disableForeignKeyConstraints();
+        Province::truncate();
+        Province::create(['provinceName' => 'Kigali City']);
+        Province::create(['provinceName' => 'Northern Province']);
+        Province::create(['provinceName' => 'Southern Province']);
+        Province::create(['provinceName' => 'Eastern Province']);
+        Province::create(['provinceName' => 'Western Province']);
+        Schema::enableForeignKeyConstraints();
     }
 }
